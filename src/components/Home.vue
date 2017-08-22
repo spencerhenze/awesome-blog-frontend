0035b38753975f7cb14153ed22dc34a1eb9eed3c
Spencer Henze <spencerhenze@gmail.com>
2017-08-22 13:41:34 -0600

    Working - with new store

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        <h1>Home Page</h1>
+        <h1>Vue is fun</h1>
+        <button @click="createNewBlog">CREATE</button>
+        <div v-if="blogs.length > 0">
+            <ul>
+                <li v-for="blog in blogs">
+                    <router-link :to="{name: 'Blog', params: {blogId: blog._id}}">{{blog.title}}</router-link>
+                </li>
+            </ul>
+        </div>
+        <div v-else>
+            <img src="https://media.tenor.com/images/85d269dc9595a7bcf87fd0fa4039dd9f/tenor.gif" alt="">
+        </div>
     </div>
 </template>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,8 @@
 import $ from 'jquery'
+import vue from 'vue'
+import vuex from 'vuex'
+
+vue.use(vuex)
 
 var ip = "//localhost:3000"
 // PRIVATE PARTS
@@ -8,12 +12,8 @@ var store = new vuex.Store({
     state: {
         blogs: [],
         activeBlog: {}
-    }
+    },
 
-
-})
-
-var store = {
     mutations: {
         addBlog(state, newBlog) {
             state.blogs.push(newBlog)
@@ -22,12 +22,14 @@ var store = {
         updateBlogs(state, blogs) {
             state.blogs = blogs;
         },
-        
+
         setActiveBlog(state, blog) {
             state.activeBlog = blog;
         }
     },
+
     actions: {
+        // post a blog
         createBlog({ commit, dispatch }, blog) {
             $.post(ip + '/api/blogs', blog).then(actualBlog => {
                 commit('addBlog', actualBlog)
@@ -36,7 +38,8 @@ var store = {
             })
         },
 
-        updateBlog({commit, dispatch}, updatedBlog) {
+        // update a blog
+        updateBlog({ commit, dispatch }, updatedBlog) {
             $.ajax({
                 url: ip + '/api/blogs/' + updatedBlog._id,
                 method: 'PUT',
@@ -45,64 +48,69 @@ var store = {
             }).then(res => {
                 dispatch.getBlogs()
             })
-            .fail(err => {
-                console.error(err)
-            })
+                .fail(err => {
+                    console.error(err)
+                })
         },
 
-        getBlogs({commit, dispatch}, blogs) {
+        // get all blogs
+        getBlogs({ commit, dispatch }, blogs) {
             $.get(ip + "/api/blogs").then(blogs => {
                 commit('updateBlogs', blogs)
             })
         },
 
-        getBlog({commit, dispatch}, blogId) {
+        // get a blog by id
+        getBlog({ commit, dispatch }, blogId) {
             $.get(ip + "/api/blogs/" + blogId).then(blog => {
                 commit('setActiveBlog', blog)
             })
         },
 
-
     }
+}) // end of store object
 
 
 
-        // state: state,
-        // search(query) {
-        //     var base = "mongodb://student:student@ds153113.mlab.com:53113/blog_db_henze"
-        //     var url = `${base}=${query}`
-
-        //     return new Promise(function (resolve, reject) {
-        //         $.get(url)
-        //             .then(data => { resolve(data) })
-        //             .catch(error => { reject(error) });
-        //     })
-        // },
-
-        getBlogs() {
-        $.get('//localhost:3000/api/blogs')
-            .then((res) => {
-                console.log(res)
-                state.blogs = res
-            })
-            .catch(() => console.log('error'))
-    },
 
 
-    addBlog(blog) {
-        $.post(ip + "//localhost:3000/api/blogs", blog)
-            .then((res) => {
-                if (res.message) {
-                    getBlogs()
-                    alert('Posted')
-                } else if (res.error) {
-                    alert("Something Broke");
-                }
 
-            })
-            .catch(() => console.log('error'))
-    }
-    // state.blogs.push(blog)   this is for saving a local copy. We are using a database because we are awesome.
-}
+// state: state,
+// search(query) {
+//     var base = "mongodb://student:student@ds153113.mlab.com:53113/blog_db_henze"
+//     var url = `${base}=${query}`
+
+//     return new Promise(function (resolve, reject) {
+//         $.get(url)
+//             .then(data => { resolve(data) })
+//             .catch(error => { reject(error) });
+//     })
+// },
+
+//         getBlogs() {
+//         $.get('//localhost:3000/api/blogs')
+//             .then((res) => {
+//                 console.log(res)
+//                 state.blogs = res
+//             })
+//             .catch(() => console.log('error'))
+//     },
+
+
+//     addBlog(blog) {
+//         $.post(ip + "//localhost:3000/api/blogs", blog)
+//             .then((res) => {
+//                 if (res.message) {
+//                     getBlogs()
+//                     alert('Posted')
+//                 } else if (res.error) {
+//                     alert("Something Broke");
+//                 }
+
+//             })
+//             .catch(() => console.log('error'))
+//     }
+//     // state.blogs.push(blog)   this is for saving a local copy. We are using a database because we are awesome.
+// }
 
 export default store
